8a63f225016fe37564a3ed1e0a7314b91d24f52c
Peter Szarvas <peter.szarvas@yahoo.com>
2017-10-11 22:11:59 +0200

    Added RequreJS, knockoutJS and basic exchangeRate module

---
+++ RealeyesHomework.UI/RealeyesHomework.UI/Scripts/app/app.js
@@ -0,0 +1,18 @@
+﻿require.config({
+    baseUrl: "/Scripts/app",
+    paths: {
+        jquery: "../jquery-2.1.1",
+        jqueryValidate: "../jquery.validate",
+        jqueryValidateUnobtrusive: "../jquery.validate.unobtrusive",
+        bootstrap: "../bootstrap",
+        ko: "/Scripts/knockout-3.4.2"
+    },
+    shim: {
+        jqueryValidate: ["jquery"],
+        jqueryValidateUnobtrusive: ["jquery", "jqueryValidate"]
+    }
+});
+
+require(["ko", "exchangeRates"], function (ko, exchangeRates) {
+    ko.applyBindings(exchangeRates, $("#home")[0]);
+});
---
+++ RealeyesHomework.UI/RealeyesHomework.UI/Scripts/app/exchangeRates.js
@@ -0,0 +1,19 @@
+﻿define(["ko"],
+    function (ko) {
+        "use strict";
+        var availableCurrencies = ko.observableArray(["HUF", "EUR"]);
+        var selectedSource = ko.observable();
+        var selectedTarget = ko.observable();
+
+        var showData = function () {
+            console.log("show data for " + selectedSource() + " and " + selectedTarget());
+        }
+
+        return {
+            availableCurrencies: availableCurrencies,
+            showData: showData,
+            selectedSource: selectedSource,
+            selectedTarget: selectedTarget
+        };
+    }
+);
